c145b8bd33caf342e0b0b84fac16cf4527d93580
MrAust1n <craig@mraustin.co.uk>
2024-06-21 16:49:20 +0100

    Boiler plate added from an old project

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.style.right === "0px") {
+    menu.style.right = "-250px";
+  } else {
+    menu.style.right = "0px";
+  }
+}
