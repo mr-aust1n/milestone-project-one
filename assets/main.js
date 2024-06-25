ed47170112de6001eed7e8f0c4c104fe13cd649a
MrAust1n <craig@mraustin.co.uk>
2024-06-25 18:57:32 +0100

    menu complete

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 function toggleMenu() {
   var menu = document.getElementById("menu");
   if (menu.style.right === "0px") {
-    menu.style.right = "-250px";
+    menu.style.right = "-750px";
   } else {
     menu.style.right = "0px";
   }
